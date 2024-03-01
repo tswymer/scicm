55dee210d5fd2f148057598b465d2aee4870f61e
Tobias Wymer <tobias.wymer@ecenta.com>
2024-03-01 22:23:31 +0100

    Added initial init script

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,30 +1,73 @@
-import { Args, Command, Flags } from '@oclif/core'
+import { Command, ux } from '@oclif/core';
+import fs from 'node:fs/promises';
+import { z } from 'zod';
+
+import { secretsSchema } from '../schemas/configuration.js';
 
 export default class Init extends Command {
-  static args = {
-    file: Args.string({ description: 'file to read' }),
-  }
+  static description = 'Initialize the the cpi-configuration-versions project.';
 
-  static description = 'describe the command here'
+  static examples = [];
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+  static flags = {};
 
-  static flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' }),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: 'n', description: 'name to print' }),
-  }
+  public async run() {
+    this.log('Welcome to the setup for a new SAP CPI configuration monitoring setup!\n');
 
-  public async run(): Promise<void> {
-    const { args, flags } = await this.parse(Init)
+    // Gather the secrets from the user
+    this.log('🔓 Local Environment Secret Setup 🔓');
+    const secrets = {
+      'CPI_URL': await ux.prompt('CPI OData API Endpoint (ex. "https://xxxxxxx-tmn.xxx.eu1.hana.ondemand.com/api/v1")'),
+      'CPI_USERNAME': await ux.prompt('CPI OData API Username'),
+      'CPI_PASSWORD': await ux.prompt('CPI OData API Password', { type: 'mask' }),
+    } satisfies z.infer<typeof secretsSchema>;
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/tobias/Documents/Development/cpi-configuration-versions/src/commands/init.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    // Create a .env string with the secrets
+    const envVariables = '# AUTOMATICALLY GENERATED\n'.concat(Object.entries(secrets).map(([key, value]) => `${key}=${value}`).join('\n'));
+
+    try {
+      // Check if the .env file exists
+      await fs.access('.env');
+
+      // If it does, append the secrets to it
+      try {
+        this.log('\nAdding secrets to existing .env file... ✅');
+        await fs.appendFile('.env', '\n\n'.concat(envVariables));
+      } catch (error) {
+        if (error instanceof Error)
+          this.error(error);
+        this.exit(1);
+      }
+    } catch {
+      // If it doesn't, create the .env file
+      this.log('\nCreating .env file with secrets... ✅');
+      await fs.writeFile('.env', envVariables);
     }
+
+    try {
+      // Check if the .gitignore file exists
+      await fs.access('.gitignore');
+
+      // If it does, check if the .env file is already in it
+      const gitignore = await fs.readFile('.gitignore', 'utf8');
+      if (gitignore.includes('.env')) {
+        this.log('.env is already part of .gitignore ✅');
+      } else {
+        // If it's not, add it
+        this.log('Adding .env to .gitignore... ✅');
+        await fs.appendFile('.gitignore', '\n.env');
+      }
+    } catch {
+      // If it doesn't, create the .gitignore file with the .env string in it
+      this.log('Creating .gitignore file with .env...');
+      await fs.writeFile('.gitignore', '.env');
+    }
+
+    this.log('\n📦 Package Monitoring Setup 📦');
+    ux.action.start('Loading integration packages from SAP CPI...');
+
+    // eslint-disable-next-line no-promise-executor-return
+    await new Promise((resolve) => setTimeout(resolve, 20_000));
   }
+
 }
--- src/schemas/configuration.ts
+++ src/schemas/configuration.ts
@@ -1,5 +1,12 @@
 import { z } from "zod";
 
+export const secretsSchema = z.object({
+    CPI_URL: z.string(),
+
+    CPI_USERNAME: z.string(),
+    CPI_PASSWORD: z.string(),
+});
+
 export const configurationSchema = z.object({
 
 });

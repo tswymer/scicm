52cd6a6247e93771b495bc53e6bebfb99fdd7683
Tobias Wymer <tobias.wymer@ecenta.com>
2024-03-03 21:51:48 +0100

    Select artifacts

--- src/commands/add/package.ts
+++ src/commands/add/package.ts
@@ -1,7 +1,7 @@
-import { select } from '@inquirer/prompts';
+import { checkbox, select } from '@inquirer/prompts';
 import { Command, ux } from '@oclif/core';
 
-import { getIntegrationPackages } from '../../utils/cpi.js';
+import { getIntegrationPackages, getIntergrationPackageDesigntimeArtifacts } from '../../utils/cpi.js';
 
 export default class Package extends Command {
     async run(): Promise<void> {
@@ -20,6 +20,25 @@ export default class Package extends Command {
             })),
         });
 
-        this.log(`Selected package: ${selectedPackageId}`);
+        const integrationDesigntimeArtifacts = await getIntergrationPackageDesigntimeArtifacts(selectedPackageId);
+
+        const selectedIntegrationArtifacts = await checkbox({
+            message: 'Select the integration artifacts to add:',
+            required: true,
+            choices: integrationDesigntimeArtifacts.map(artifact => ({
+                value: artifact.Id,
+                name: `[${artifact.Id}]:\t${artifact.Name ?? '_Unnamed_Artifact_'}`,
+                checked: true,
+            })),
+            theme: {
+                style: {
+                    renderSelectedChoices(selectedChoices: unknown[], allChoices: unknown[]) {
+                        return `Selected ${selectedChoices.length}/${allChoices.length} artifacts`;
+                    },
+                }
+            }
+        });
+
+        this.log('Selected artifacts:', selectedIntegrationArtifacts);
     }
 }
--- src/utils/cpi.ts
+++ src/utils/cpi.ts
@@ -4,11 +4,19 @@ import { integrationContent } from "../cpi-odata/IntegrationContent/index.js";
 import { cpiEnvironment, getConfiguration } from "./configuration.js";
 import { getSecrets, secretsSchema } from "./secrets.js";
 
-const { integrationPackagesApi, } = integrationContent();
+const { integrationPackagesApi } = integrationContent();
 
 function buildCPIODataURL({ accountShortName, sslHost, region, }: z.infer<typeof cpiEnvironment>) {
     return `https://${accountShortName}-tmn.${sslHost}.${region}/api/v1`;
 }
+
+const integrationDesigntimeArtifactsSchema = z.array(z.object({
+    Id: z.string(),
+    Version: z.string(),
+    PackageId: z.string().optional(),
+    Name: z.string().optional(),
+    Description: z.string().optional(),
+}));
 
 export async function testCredentials(environment: z.infer<typeof cpiEnvironment>, secrets: z.infer<typeof secretsSchema>) {
     await integrationPackagesApi.requestBuilder()
@@ -20,21 +28,42 @@ export async function testCredentials(environment: z.infer<typeof cpiEnvironment
         });
 }
 
-export async function getIntegrationPackages() {
+async function getExecutionDestination() {
     const configuration = await getConfiguration();
     const secrets = await getSecrets();
 
-    const integrationPackages = integrationPackagesApi.requestBuilder()
-        .getAll()
-        .execute({
-            url: buildCPIODataURL({
-                accountShortName: configuration.environment.accountShortName,
-                region: configuration.environment.region,
-                sslHost: configuration.environment.sslHost,
-            }),
-            username: secrets.CPI_USERNAME,
-            password: secrets.CPI_PASSWORD,
-        });
+    return {
+        url: buildCPIODataURL({
+            accountShortName: configuration.environment.accountShortName,
+            region: configuration.environment.region,
+            sslHost: configuration.environment.sslHost,
+        }),
+        username: secrets.CPI_USERNAME,
+        password: secrets.CPI_PASSWORD,
+    } as const;
+}
 
-    return integrationPackages;
+export async function getIntegrationPackages() {
+    return integrationPackagesApi.requestBuilder()
+        .getAll()
+        .execute(await getExecutionDestination());
+}
+
+export async function getIntergrationPackageDesigntimeArtifacts(integrationPackageId: string) {
+    const response = await integrationPackagesApi.requestBuilder()
+        .getByKey(integrationPackageId)
+        .appendPath('/IntegrationDesigntimeArtifacts')
+        .executeRaw(await getExecutionDestination());
+
+    if (response.status !== 200) {
+        throw new Error(`Failed to get integration package designtime artifacts: ${response.status} - ${response.statusText}`);
+    }
+
+    const results = response?.data?.d?.results;
+
+    if (!results || !Array.isArray(results)) {
+        throw new Error('Invalid /IntegrationDesigntimeArtifacts response from CPI');
+    }
+
+    return integrationDesigntimeArtifactsSchema.parse(results);
 }
